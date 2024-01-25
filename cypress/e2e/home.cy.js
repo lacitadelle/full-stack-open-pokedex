fb91577c45f1dbbcae12f502a252e155e025ae12
lacitadelle <jkomala99@gmail.com>
2024-01-26 02:10:19 +0700

    added e2e test for navigation from home to a pokemon

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,7 +1,17 @@
+beforeEach(function () {
+  cy.visit('http://localhost:5000')
+})
+
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
+
+describe('Page navigation', function () {
+  it('ensures the right Pokemon gets opened', function () {
+    cy.contains('charmander').click()
+    cy.contains('blaze')
   })
 })
